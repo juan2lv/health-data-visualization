d1ca9147567ea64c79b84720184a3ffe9e66b8e6
juan2lv <juan2lv@gmail.com>
2015-07-17 22:07:37 +0800

    line chart
    line chart

--- scatterchart.js
+++ scatterchart.js
@@ -1,5 +1,5 @@
-var data = [[[5,0,300,"ICD10"], [10,17,200,"ICD9"], [15,4,50,"ICD10"], [2,8,50,"ICD9"], [10,0,20,"ICD9"]],
-            [[5,1,100,"ICD10"], [10,17,200,"ICD9"], [15,4,50,"ICD10"], [2,8,50,"ICD9"], [10,0,20,"ICD9"]]];
+var datas = [[[5,0,300,"ICD10"], [10,17,200,"ICD9"], [15,4,50,"ICD10"], [2,8,50,"ICD9"], [10,0,20,"ICD9"]],
+            [[5,1,10,"ICD10"], [10,17,200,"ICD9"], [15,4,50,"ICD10"], [2,8,50,"ICD9"], [10,0,200,"ICD9"]]];
    
 var margin = {top: 20, right: 15, bottom: 60, left: 60}
 	, width = 1000 - margin.left - margin.right
@@ -8,15 +8,15 @@ var margin = {top: 20, right: 15, bottom: 60, left: 60}
 var padding = 50;
 
 var x = d3.scale.linear()
-	.domain([0, d3.max(data, function (d) { return d[0]; })])
+	.domain([0, d3.max(datas[0], function (d) { return d[0]; })])
 	.range([ padding, width-50 ]);
 
 var y = d3.scale.linear()
-	.domain([0, d3.max(data, function(d) { return d[1]; })])
+	.domain([0, d3.max(datas[0], function(d) { return d[1]; })])
 	.range([ height-padding, padding ]);
 
 var r = d3.scale.linear()
-	.domain([0, d3.max(data, function(d) { return d[2]; })])
+	.domain([0, d3.max(datas[0], function(d) { return d[2]; })])
 	.range([ 0, padding ]);
 
 var chart = d3.select('#content')
@@ -53,12 +53,30 @@ main.append('g')
 
 var g = main.append("svg:g"); 
 
-g.selectAll("circle")
-	.data(data)
-		.enter().append("circle")
+update(datas[0]);
+
+function update(data) {
+	var circles = g.selectAll("circle").data(data);
+	var circlesEnter = circles.enter().append("circle")
 		.attr("cx", function (d) { return x(d[0]); } )
 		.attr("cy", function (d) { return y(d[1]); } )
 		.attr("r", function (d) { return r(d[2]); } )
 		.attr("class", function (d) { return d[3];});
+	var circlesExit = circles.exit().remove();
+	circlesExit.selectAll("circle").attr("r", 0);
+	console.log("update");
+}
+
+var count = 0;
+
+document.getElementById('animate').addEventListener('click', function () {
+	count++;
+	var i=count%2;
+	update(datas[i]);
+	console.log(i);
+})
+
+
+
 
 //g.selectAll("circle").transition().duration(2000).attr("cy", "50%");
